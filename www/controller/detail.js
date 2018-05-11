aa17db94129c7ce0002c800c4ad9376a14f8f409
juliet26 <36687161+juliet26@users.noreply.github.com>
2018-05-10 21:00:29 -0500

    Update detail.js

--- www/controller/detail.js
+++ www/controller/detail.js
@@ -4,12 +4,11 @@ angular.module('starter').controller('detailController', function($state){
 	switch (personajesName){
 	case 'El chavo':
 	vm.detalle = {
-		nombre: 'El chavo'
-		edad: '8'
-		vivienda: 'La vecindad'
-		fotoUrl: 'img/chavo.jpg'
-		resumen: 'El Chavo del 8 es una serie de televisión cómica mexicana creada y protagonizada por Roberto Gómez Bolaños, 
-		producida por Televisión Independiente de México. Fue emitida por primera vez el 20 de junio de 1971 por Canal 8.​ '
+		nombre: 'El chavo',
+		edad: '8',
+		vivienda: 'La vecindad',
+		fotoUrl: 'img/chavo.jpg',
+		resumen: 'El Chavo del 8 es una serie de televisión cómica mexicana creada y protagonizada por Roberto Gómez Bolaños, producida por Televisión Independiente de México. Fue emitida por primera vez el 20 de junio de 1971 por Canal 8.'
 	};
 	}
 	//boton anterior
